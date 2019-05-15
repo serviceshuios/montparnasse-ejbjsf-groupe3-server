aba6dac4f57067a370fed396129fcadfec695973
Raph <raphael_bruneau@orange.fr>
2019-05-15 14:53:22 +0200

    Push couche Ejb

---
+++ ejbModule/com/infotel/ejb/ILotissementRemote.java
@@ -0,0 +1,32 @@
+package com.infotel.ejb;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import com.infotel.metier.Lotissement;
+import com.infotel.metier.Personne;
+
+@Remote
+public interface ILotissementRemote {
+	
+	public void ajouterPersonne(Personne p);
+	public List<Personne> findAllPersonne();
+	public void modifierPersonne(Personne p);
+	public void supprimerPersonne(Personne p);
+	public Personne getPersonne(Long idPersonne);
+	public Personne affichagePersonne(Long idPersonne);
+	
+	public void ajouterLotissement(Lotissement l);
+	public List<Lotissement> findAllLotissement();
+	public void modifierLotissement(Lotissement l);
+	public void supprimerLotissement(Lotissement l);
+	public Lotissement getLotissement(Long idLot);
+	public Lotissement affichageLotissement(Long idLot);
+	
+	
+	
+	public void acheterLotissement(Personne p, Lotissement l);
+	public void seMarier (Personne p1, Personne p2);
+
+}
